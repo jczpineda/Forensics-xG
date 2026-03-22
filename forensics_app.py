c0142c10ab5b192ac120c6c82ae4dacfaaa8db5e
Juan Carlos Pineda <jczpineda@gmail.com>
2026-03-22 15:14:36 +0100

    Add AFC Bournemouth stats file to forensics_app.py

--- forensics_app.py
+++ forensics_app.py
@@ -250,7 +250,8 @@ CASE_DATABASE = {
             "vs West Ham (2)": "carrick.json/West Ham 2.JSON",
             "vs Everton (2)": "carrick.json/Everton 2.JSON",
             "vs Newcastle United (2)": "carrick.json/Newcastle United 2.JSON",
-            "vs Crystal Palace (2)": "carrick.json/Crystal Palace 2.JSON"
+            "vs Crystal Palace (2)": "carrick.json/Crystal Palace 2.JSON",
+            "vs AFC Bournemouth (2)": "carrick.json/AFC Bournemouth 2.JSON"
         },
         "stats_files": {}
     }
